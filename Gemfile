93e60b36f88aa4936839bd33df08a41832b8e12e
Nr <nofrazier3@gmail.com>
2015-06-22 16:01:02 -0700

    Added signup and login pages

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ gem 'rack-livereload', group: :development
 gem 'guard-bundler', group: :development
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   resources "contacts", only: [:new, :create]
   get '/contact' => 'contacts#new'
   get '/about' => 'pages#about'
+
+  get 'signup' => 'users#new'
+  resources :users
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
