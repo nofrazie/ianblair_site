0ee3f197e7a50d9e8eff54b3fd4480cf81f84d16
Nolan Frazier <nofrazier3@gmail.com>
2015-06-22 22:41:17 -0700

    Added instafeed fixed box sizing

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150622221509) do
 
-  create_table "contacts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
